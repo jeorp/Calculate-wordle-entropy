f4813c510f75e81dc9a5842c6b975e17b37faf07
jeorp <jeorpqxx@gmail.com>
2022-01-28 23:10:04 +0900

    finish implement wordle entropy

--- app/Main.hs
+++ app/Main.hs
@@ -1,69 +1,10 @@
 module Main where
 
---import qualified Data.ByteString.Lazy.Char8 as BL
-import Data.Char
-import Data.List
-import qualified Data.List.Key as LTH
-import Data.Maybe 
-import Control.Arrow 
+import Wordle
+import Data.List (intercalate)
 
-main :: IO ()
-main = ranking "wordle.txt" >>= mapM_ print . take 100
-
-validateN :: Int -> FilePath -> FilePath -> IO ()
-validateN n input output = do
-  file <- readFile input
-  let ws = words file
-      five_words = filter (\s -> all isAlpha s && length s == n) ws
-  print $ length ws
-  writeFile output $ intercalate "\n" five_words
-
-wordEntropyAverage :: String -> Double
-wordEntropyAverage s = sum $ uncurry (*) . (charPossibility &&& charEntropy) <$> s
-
- 
-possibilityMap :: [(Char, Double)]
-possibilityMap = 
- [
-  ('a', 0.0575),
-  ('b', 0.0128),
-  ('c', 0.0263),
-  ('d', 0.0285),
-  ('e', 0.0913),
-  ('f', 0.0173),
-  ('g', 0.0133),
-  ('h', 0.0313),
-  ('i', 0.0599),
-  ('j', 0.0006),
-  ('k', 0.0084),
-  ('l', 0.0335),
-  ('m', 0.0235),
-  ('n', 0.0596),
-  ('o', 0.0689),
-  ('p', 0.0192),
-  ('q', 0.0008),
-  ('r', 0.0508),
-  ('s', 0.0567),
-  ('t', 0.0706),
-  ('u', 0.0334),
-  ('v', 0.0069),
-  ('w', 0.0119),
-  ('x', 0.0073),
-  ('y', 0.0164),
-  ('z', 0.0007) 
- ]
-
-charPossibility :: Char -> Double
-charPossibility c = if isAlpha c
-  then fromMaybe 1  $ lookup (toLower c) possibilityMap
-  else 1
-
-charEntropy :: Char -> Double
-charEntropy = negate . logBase 2 . charPossibility
-
-ranking :: FilePath -> IO [(String, Double)]
-ranking path = do
-  file <-readFile path
-  let ws = words file
-      res = (id &&& wordEntropyAverage) <$> ws
-  return $ LTH.sort (negate . snd) res
+main = do
+  putStrLn "Please wait ..."
+  wordleEntropyRanking "wordle.txt" 
+  >>= writeFile "wordleEntropyRanking.txt" . intercalate "\n" . fmap fst . take 100
+  >> putStrLn "Finish!"
--- src/Wordle.hs
+++ src/Wordle.hs
@@ -1,13 +1,21 @@
 module Wordle where
 
-
-import Data.Char
-import Data.List
+import Control.Concurrent.Async
 import qualified Data.List.Key as LTH
-import Data.Maybe 
-import Control.Arrow 
+import qualified Data.Map as M
+ 
+import Data.Monoid (Endo(Endo, appEndo))
+import Control.Arrow (Arrow((&&&))) 
 
 data Color = Gray | Yellow | Green deriving(Show, Eq)
+
+instance Ord Color where
+  compare Green Green = EQ
+  compare Green _ = GT
+  compare Yellow Yellow = EQ
+  compare Yellow _ = GT
+  compare Gray Gray = EQ
+  compare _ _ = LT
 
 judgeWordle :: String -> String -> [Color]
 judgeWordle a b = if length a == length b then cal a b else []
@@ -20,5 +28,29 @@ judgeWordle a b = if length a == length b then cal a b else []
 
     cal a b = reverse $ cal' a a b []
 
+insertColor :: [Color] -> M.Map [Color] Int -> M.Map [Color] Int
+insertColor color = M.insertWith (+) color 1
+
+countColors :: String -> [String] -> M.Map [Color] Int -> M.Map [Color] Int
+countColors s xs = appEndo $ foldMap (Endo . insertColor . judgeWordle s) xs
+              
 wordleEntropyAverage :: String -> [String] -> Double
-wordleEntropyAverage s dict = undefined
+wordleEntropyAverage s dict_ = 
+  let dict = filter (/= s) dict_
+      sampleCount = length dict 
+      maps = countColors s dict M.empty
+  in M.foldr (\a b -> let p = fromIntegral a / fromIntegral sampleCount in p*negate (logBase 2 p) + b) 0 maps
+
+calculateWordleEntropy :: String -> FilePath -> IO Double
+calculateWordleEntropy s path = do
+  file <- readFile path
+  let ws = words file
+  pure $ wordleEntropyAverage s ws
+
+wordleEntropyRanking :: FilePath -> IO [(String, Double)]
+wordleEntropyRanking path = do
+  file <- readFile path
+  let ws = words file
+      f = id &&& flip wordleEntropyAverage ws
+  LTH.sort (negate . snd) <$> mapConcurrently (pure . f) ws
+  
