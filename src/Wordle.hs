8c789f5d498bf2eb5a0d4b12ef6f2ead08c4dc95
jeorp <jeorpqxx@gmail.com>
2022-01-29 22:24:56 +0900

    update

--- src/Wordle.hs
+++ src/Wordle.hs
@@ -106,7 +106,7 @@ wordleEntropyRankingConcurrent path = do
 
 
 -- dynamic version is very slow ...
--- Maybe, reading TVar is not efficient.  
+-- Maybe, readTVar is not efficient.  
 
 wordleEntropyRankingDynamic :: FilePath -> IO [(String, Double)]
 wordleEntropyRankingDynamic path = do
